ceca2b75876c2e1e72125acc87d95ecefefb53ed
yuxiangxin <1456832376@qq.com>
2021-05-24 00:56:01 +0800

    rm cached

--- command/build/libs/atrans.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-REM Atrans(Android xml translate to Harmony) start bat
-REM Version: v1.0
-REM Build date: 2021-5-24
-
-JAVA -jar .\atrans-command-1.0.jar %cd% %1 %2 %3 %4 %5 %6 %7 %8 %9 
